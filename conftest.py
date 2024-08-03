340b363ad170508eb233e9bd698d482317509ebc
ChebanovAlexsandr <chebanov.al23@yandex.ru>
2024-08-03 14:52:11 +0300

    "Поменял драйвер"

--- conftest.py
+++ conftest.py
@@ -1,40 +1,49 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.options import Options
+
 from pages.sale_page import SalePage
 from pages.customer_login import CustomerLogin
 from pages.eco_friendly_page import EcoPage
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
-class Chrome(WebDriver):
+# class Chrome(WebDriver):
+#
+#     def __init__(self, remote_host, remote_port):
+#         chrome_options = webdriver.ChromeOptions()
+#         chrome_options.add_argument('--headless')
+#         chrome_options.add_argument('window-size=1920,1080')
+#         chrome_options.add_argument('--no-sandbox')
+#         chrome_options.add_argument('--disable-dev-shm-usage')
+#         chrome_options.add_argument('--ignore-ssl-errors=yes')
+#         chrome_options.add_argument('--ignore-certificate-errors')
+#
+#         super().__init__(
+#             command_executor=f"http://{remote_host}:{remote_port}/wd/hub",
+#             options=chrome_options
+#         )
 
-    def __init__(self, remote_host, remote_port):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('window-size=1920,1080')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--ignore-ssl-errors=yes')
-        chrome_options.add_argument('--ignore-certificate-errors')
 
-        super().__init__(
-            command_executor=f"http://{remote_host}:{remote_port}/wd/hub",
-            options=chrome_options
-        )
+# @pytest.fixture
+# def driver():
+#     browser = Chrome(
+#         remote_host="localhost",
+#         remote_port=4444
+#     )
+#     browser.implicitly_wait(10)
+#     browser.maximize_window()
+#     yield browser
+#     browser.quit()
 
-
-@pytest.fixture
+@pytest.fixture()
 def driver():
-    browser = Chrome(
-        remote_host="localhost",
-        remote_port=4444
-    )
-    browser.implicitly_wait(10)
-    browser.maximize_window()
-    yield browser
-    browser.quit()
-
-
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('window-size=1920,1080')
+    chrome_driver = webdriver.Chrome(options=options)
+    chrome_driver.implicitly_wait(5)
+    yield chrome_driver
 @pytest.fixture()
 def sale_page(driver):
     return SalePage(driver)
